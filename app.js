fddf48a7a24a960a0ce437733d3990f36805c973
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-25 12:22:33 +0500

    js ass 16-20

---
+++ app.js
@@ -0,0 +1,87 @@
+// Q1=============
+// let mult = [[]]
+// Q2=============
+// let arr = [[0, 1, 2, 3],[1, 0, 1, 2],[2, 1, 0, 1]];
+//   for(let i = 0 ; i < arr.length ; i++){
+    //     for(let j = 0; j < arr[i].length; j++){
+        //         document.write(arr[i][j]+ " ")
+        //     }
+        //     document.write("<br> ")
+        //   }
+ // Q3=============
+ //  for(let i = 1; i <=10 ; i++){
+     //     document.write(i + "<br>")
+     //  }
+// Q4=================
+// let table = +prompt("Enter your table")
+// let start = +prompt("Enter your table start")
+// let len = +prompt("Enter your table length")
+// for(let i = start; i <= len ; i++){
+    //     console.log(table + "x " + i + "= " + table*i)
+    // }
+    // Q5====================
+    // let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+    // for(let i= 0 ; i < fruits.length ; i++){
+    //     console.log("Element at index "+ i +" is "+ fruits[i])
+    // }
+    // Q6============================
+    // for(let i = 1 ; i <= 15 ; i++){
+    //  document.write(i + "<br>")
+    // }
+// reverse counting 10-1
+// for (let i = 10 ; i >= 1 ; i--){
+//           document.write(i + "<br>")
+// }
+// even number===========0-20
+// for(let i = 0 ; i <= 20 ; i++ ){
+// if(i % 2 === 0){
+
+//     console.log(i+ " is even")
+//   }
+// }
+
+// for(let i = 0 ; i <= 20 ; i++ ){
+//     if(i % 2 !== 0){
+    
+//         console.log(i+ " is Odd")
+//       }
+//     }
+// Q6====================
+// let a =["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"]
+// for(let i = 0 ; i < a.length ; i++){
+
+//     document.write(a[i])
+// }
+// Q7=======================
+
+// let user = prompt('enter your item')
+
+// let itemFound = false
+// for(let i = 0 ; i < a.length ; i++){
+//     if(user === a[i]){
+//         alert(a[i]+ " is available at index "+ i+" in our bakery" )
+//         itemFound = true;
+//         break;
+//     }
+// }
+// if(!itemFound){
+//     alert("We are sorry, " + user + " is not available in our bakery");   
+//     }
+// Q8===========================
+// let numbers = [24, 53, 78, 91, 12];
+// let maxNumber = Math.max(...numbers)
+
+// console.log( "Array ia " + numbers)
+// console.log("Maximum number is: " + maxNumber);
+// // Q9=============================
+// let numbers = [24, 53, 78, 91, 12];
+// let maxNumber = Math.min(...numbers)
+
+// console.log( "Array ia " + numbers)
+// console.log("Maximum number is: " + maxNumber);
+// Q9==========================
+// for(let i = 5 ; i <= 100 ; i += 5){
+//     console.log(i)
+// }
+// JAVASCRIPT ASSIGNMENT 16-20 has been completed=============
